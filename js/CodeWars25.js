dd46390b8d3c27f06bdfff65cb54732c2b8a853c
Nicole B <63973798+NicoleBazile@users.noreply.github.com>
2020-09-10 23:52:08 -0400

    Most digits

---
+++ js/CodeWars25.js
@@ -0,0 +1,10 @@
+// Find the number with the most digits.
+//
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+function findLongest(array){
+  var newArr = array.slice().sort(function(a,b){
+   return b.toString().length-a.toString().length;
+ });
+ return newArr[0];
+}
