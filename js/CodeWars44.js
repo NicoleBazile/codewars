5bdbfe81fc834d5f3581480cb9ac045dc3671cb9
Nicole B <63973798+NicoleBazile@users.noreply.github.com>
2020-11-05 11:37:52 -0500

    Reduce but Grow

---
+++ js/CodeWars44.js
@@ -0,0 +1,8 @@
+
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+
+function grow(x){
+let total = x.reduce((a,b) => a*b)
+ return total
+}
